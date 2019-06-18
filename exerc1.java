3812a5ce9139bdd847da67a32baf24ad25621fca
GabrielGGA <gabriegomes.alves@gmail.com>
2019-06-18 12:30:06 -0300

    ex1

---
+++ exerc1.java
@@ -0,0 +1,22 @@
+package tiaedna1;
+import java.util.Scanner;
+public class exerc1 {
+
+	public static void main(String[] args) {
+	Scanner tec = new Scanner(System.in);	
+int x;
+x = tec.nextInt();
+if (x < 15) {
+	System.out.println("seu numero é menor que 15");
+}
+else if (x > 100) {
+	System.out.println("seu numero é maior que 100");
+	}
+else if (x > 15 && x < 100 ); {
+	System.out.println("digite outro numero ;)");}
+    {
+	
+}
+    
+	}
+}
